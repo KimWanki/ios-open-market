c5e2c240820642e11151f3a2aa48b1f07dc02401
KimWanki <seoulkwk95@gmail.com>
2021-08-18 21:22:28 +0900

    feat: 사진 전송을 위한 Media 타입 구현

---
+++ OpenMarket/OpenMarket/Model/Media.swift
@@ -0,0 +1,24 @@
+//
+//  Media.swift
+//  OpenMarket
+//
+//  Created by Luyan on 2021/08/17.
+//
+
+import UIKit
+
+struct Media {
+    let key: String
+    let filename: String
+    let data: Data
+    let mimeType: String
+
+    init?(withImage image: UIImage, forKey key: String) {
+        self.key = key
+        self.mimeType = "image/jpeg"
+        self.filename = "photo\(arc4random()).jpeg"
+
+        guard let data = image.jpegData(compressionQuality: 0.7) else { return nil }
+        self.data = data
+    }
+}
