ec31f9efe00da4f48b774371c81a34afcf7ad3e6
KimWanki <seoulkwk95@gmail.com>
2021-08-18 21:25:58 +0900

    feat: sessionConponent 프로토콜 정의, request Method 구현

--- OpenMarket/OpenMarket/SupportingFiles/NetworkHandler.swift
+++ OpenMarket/OpenMarket/SupportingFiles/NetworkHandler.swift
@@ -7,6 +7,52 @@
 
 import Foundation
 
+protocol sessionComponent {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: sessionComponent {}
+
 struct NetworkHandler {
+    private let session: sessionComponent
     
+    init(session: sessionComponent) {
+        self.session = session
+    }
+    
+    func request(with http: HttpInfo, form: HttpBody? = nil, completionHandler: @escaping () -> Void) {
+        guard let url = URL(string: http.Info.url) else { return }
+        var urlRequest = URLRequest(url: url)
+        
+        let contentType = form?.contentType ?? "application/json"
+        urlRequest.httpMethod = http.Info.type.description
+        
+        if let httpBody = form?.createBody() {
+            if let boundary = form?.boundary {
+                urlRequest.setValue(contentType + "; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+            } else {
+                urlRequest.setValue(contentType, forHTTPHeaderField: "Content-Type")
+            }
+            urlRequest.httpBody = httpBody
+        }
+        
+        session.dataTask(with: urlRequest) { data, response, error in
+            if let response = response {
+                print(response)
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+        completionHandler()
+    }
+    
+    func generateBoundary() -> String {
+        return "Boundary-\(NSUUID().uuidString)"
+    }
 }
