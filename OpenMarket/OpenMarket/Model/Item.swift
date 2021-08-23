484c551207b71a51f017bb59afb82e7a0e3afe7b
KimWanki <seoulkwk95@gmail.com>
2021-08-23 12:43:54 +0900

    refactor: 모델 구조를 struct에서 class의 상속을 이용한 방식으로 변경
    optional로 인한 옵셔널바인딩 반복 줄이기 위한 class의 상속 구조로 변경

--- OpenMarket/OpenMarket/Model/Item.swift
+++ OpenMarket/OpenMarket/Model/Item.swift
@@ -7,15 +7,7 @@
 
 import Foundation
 
-struct Item: Codable {
-    let id: Int
-    let title: String
-    let descriptions: String?
-    let price: Int
-    let currency: String
-    let stock: Int
-    let discountedPrice: Int?
-    let thumbnails: [String]
-    let images: [String]?
-    let registrationDate: Double
+final class Item: CollectionItem {
+    let descriptions: String = ""
+    let images: [String] = []
 }
--- OpenMarket/OpenMarket/Model/ItemCollection.swift
+++ OpenMarket/OpenMarket/Model/ItemCollection.swift
@@ -5,9 +5,20 @@
 //  Created by Luyan, Ellen on 2021/08/10.
 //
 
-import Foundation
+import UIKit
 
-struct ItemCollection: Codable {
+class CollectionItem: Decodable {
+    let id: Int
+    let title: String
+    let price: Int
+    let currency: String
+    let stock: Int
+    let discountedPrice: Int?
+    let thumbnails: [String]
+    let registrationDate: Double
+}
+
+class ItemCollection: Decodable {
     let page: Int
-    let items: [Item]
+    let items: [CollectionItem]
 }
